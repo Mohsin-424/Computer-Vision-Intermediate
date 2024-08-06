0d635987c6cbcd34d26be7a45f539c8a06fb8a46
Muhammad Mohsin <mohsinpu24@gmail.com>
2024-08-06 13:30:23 +0500

    Object-Person

--- Object-Person.py
+++ Object-Person.py
@@ -1,28 +1,32 @@
 import os
 import cv2
 
-video_path = os.path.join('.', 'data', 'people.mp4')
+video_path = os.path.join('.' , 'data' , 'people.mp4')
 
 cap = cv2.VideoCapture(video_path)
 
+
 if not cap.isOpened():
-    print('Error in video playing')
+    print('Error in video playing ')
     exit()
 
-print("Video is opened successfully")
-
+print("Video is opened Sucessfully")
 while True:
     ret, frame = cap.read()
     if not ret:
         print('No more frames to detect')
-        break  # Use break instead of exit to ensure proper cleanup
+        exit()
+        
+    cv2.imshow( 'Frame ' , frame )
     
-    cv2.imshow('Frame', frame)
     
-    # Check for 'q' key press to exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1)& 0xFF == ord('q'):
         print("Exiting")
         break
-
+    
+    
+    
+    
+    
 cap.release()
 cv2.destroyAllWindows()
